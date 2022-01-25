dd34d2edc41b5727a14ad9f459e21738ba27393d
EugenioLuisSoria <eugeniosoria@gmail.com>
2022-01-25 11:23:51 -0300

    subiendo...

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const publicPath =path.resolve(__dirname, "public/")
 app.use(express.static(publicPath))
 
 
-app.listen(3000,()=> console.log("Si, todo bien!"));
+app.listen(process.env.PORT || 3000,()=> console.log("Server Running"));
 
 app.get("/", function(req,res){
     res.sendFile(path.resolve(__dirname,"views/","home.html"))
